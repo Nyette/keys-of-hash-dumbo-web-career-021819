7944c55b67ad5de77826441857cb4bf245ccd098
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-09 01:32:12 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,11 +4,13 @@ class Hash
   def keys_of(*arguments)
     # returns an array with every key from the hash
     # whose value matches the value(s) given as an argument
+    keys = []
     arguments.each do |argument|
       if value == argument
         binding.pry
         p key
       end
     end
+  keys
   end
 end
