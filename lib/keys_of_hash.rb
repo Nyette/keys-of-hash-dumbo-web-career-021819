dcf8790f004aee9087404d4509bbeadb38d65cbe
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-09 01:29:25 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -5,12 +5,10 @@ class Hash
     # returns an array with every key from the hash
     # whose value matches the value(s) given as an argument
     arguments.each do |argument|
-      if value == arguments
+      if value == argument
         binding.pry
         p key
       end
     end
   end
 end
-
-:animals.keys_of("Panama").class
